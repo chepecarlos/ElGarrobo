7869f63807d32c62f79755654319009f4061c209
ChepeCarlos <chepecarlos@alswblog.org>
2020-02-16 20:05:45 -0600

    Codigo Base pruevas de carpetas

---
+++ ElGatoALSW.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+# Librerias
+import os
+import threading
+from PIL import Image, ImageDraw, ImageFont
+from StreamDeck.DeviceManager import DeviceManager
+from StreamDeck.ImageHelpers import PILHelper
+
+# Recusos para sistema
+FolderRecursos = os.path.join(os.path.dirname(__file__), "Recursos")
+
+# Variables Globass
+Carpeta = ["Arduino","Blender"]
+
+def ObtenerImagen(deck, key, state):
+    name = "{}".format(Carpeta[key])
+    icon = "{}{}.png".format(Carpeta[key],"P" if state else "R")
+    font = "Roboto-Regular.ttf"
+    label = "{}".format(Carpeta[key])
+
+    return {
+        "name": name,
+        "icon": os.path.join(FolderRecursos, icon),
+        "font": os.path.join(FolderRecursos, font),
+        "label": label
+    }
+
+#
+def CargarImagen(deck, icon_filename, font_filename, label_text):
+    # Create new key image of the correct dimensions, black background.
+    image = PILHelper.create_image(deck)
+
+    # Resize the source image asset to best-fit the dimensions of a single key,
+    # and paste it onto our blank frame centered as closely as possible.
+    icon = Image.open(icon_filename).convert("RGBA")
+    icon.thumbnail((image.width, image.height - 20), Image.LANCZOS)
+    icon_pos = ((image.width - icon.width) // 2, 0)
+    image.paste(icon, icon_pos, icon)
+
+    # Load a custom TrueType font and use it to overlay the key index, draw key
+    # label onto the image.
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype(font_filename, 14)
+    label_w, label_h = draw.textsize(label_text, font=font)
+    label_pos = ((image.width - label_w) // 2, image.height - 20)
+    draw.text(label_pos, text=label_text, font=font, fill="white")
+
+    return PILHelper.to_native_format(deck, image)
+
+
+# Actualizar
+def CambiarImagen(deck, key, state):
+    print("Cargando {}".format(Carpeta[key]))
+
+    # Obtener Imagen
+    ImagenEstilo = ObtenerImagen(deck, key, state)
+
+    #
+    Imagen = CargarImagen(deck, ImagenEstilo["icon"], ImagenEstilo["font"], ImagenEstilo["label"])
+
+    deck.set_key_image(key, Imagen)
+
+# Principal
+if __name__ == "__main__":
+
+    # Buscando Dispisitovos
+    streamdecks = DeviceManager().enumerate()
+
+    print("Programa El Gato ALSW - {}".format("Encontrado" if len(streamdecks) > 0 else "No Conectado"));
+
+    for index, deck in enumerate(streamdecks):
+
+        # Abriendo puerto
+        deck.open()
+        deck.reset()
+
+        print("Abriendo '{}' dispositivo (Numero Serial: '{}')".format(deck.deck_type(), deck.get_serial_number()))
+
+        # Cambiar Brillo
+        deck.set_brightness(50)
+
+        for key in range(len(Carpeta)):
+            CambiarImagen(deck, key, False)
+
+        # Sistema de Coalbask
+        for t in threading.enumerate():
+            if t is threading.currentThread():
+                continue
+
+            if t.is_alive():
+                t.join()
---
+++ Read.md
@@ -0,0 +1,41 @@
+# Principal
+
+Las opciones disponibles para menu principal
+
+* Arduino
+* OBS_Striming
+* Blender
+* Gromit-MPX
+* Inskcape
+
+# Arduino
+
+0. Regresar
+1. Verificar
+2. Compilar
+3. Monitor Serial
+4. Serial Ploter
+5. Auto Formato
+6. Nuevo
+7. Abir
+8. Guardar
+9. Preferencia
+
+## OBS_Striming
+
+0. Regreso
+1. Inicial/Parar Striming
+2. Inicial/Parar Grabacion
+3. Iniciando Striming
+4. Cambiar a Esena principal
+5. Cambiar a Esena PC
+6. Terminando Striming Gracias
+7. Mute Microfono
+8. Configurar
+
+## Blender
+
+0. Regreso
+1. Guardas
+
+## Inskcape
