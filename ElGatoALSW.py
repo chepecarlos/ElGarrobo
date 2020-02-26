ca81ef5eaf6f00d0ccc103cbf62f4bf759b22342
ChepeCarlos <chepecarlos@alswblog.org>
2020-02-25 18:29:16 -0600

    Agregando mas funcionas con in

--- ElGatoALSW.py
+++ ElGatoALSW.py
@@ -25,73 +25,6 @@ Estado = -1
 with open('Comandos.json') as f:
     data = json.load(f)
 
-Comandos = [["Arduino", [["Verificar", [Key.ctrl, "r"]], ["Salvar", [Key.ctrl, "s"]]]]]
-
-
-
-def ObtenerImagen(deck, key, state):
-
-    if Estado == -1:
-        name = "{}".format(data['Comando'][key]['Nombre'])
-        icon = "{}{}.png".format(Comandos[key][0],"P" if state else "R")
-        font = "Roboto-Regular.ttf"
-        label = "{}".format(Comandos[key][0])
-    else:
-        if key == deck.key_count() -1:
-            name = "Salir"
-            icon = "Salir.png"
-            font = "Roboto-Regular.ttf"
-            label = "Salir"
-        else:
-            name = "{}".format(Comandos[Estado][1][key][0])
-            icon = "{}/{}{}.png".format(Comandos[Estado][0], Comandos[Estado][1][key][0], "P" if state else "R")
-            font = "Roboto-Regular.ttf"
-            label = "{}".format(Comandos[Estado][1][key][0])
-
-    return {
-        "name": name,
-        "icon": os.path.join(FolderRecursos, icon),
-        "font": os.path.join(FolderRecursos, font),
-        "label": label
-    }
-
-#
-def CargarImagen(deck, icon_filename, font_filename, label_text, titulo = False):
-    # Create new key image of the correct dimensions, black background.
-    image = PILHelper.create_image(deck)
-    # Resize the source image asset to best-fit the dimensions of a single key,
-    # and paste it onto our bCargarImagenlank frame centered as closely as possible.
-    icon = Image.open(icon_filename).convert("RGBA")
-    if titulo:
-        icon.thumbnail((image.width, image.height - 20), Image.LANCZOS)
-    else:
-        icon.thumbnail((image.width, image.height), Image.LANCZOS)
-    icon_pos = ((image.width - icon.width) // 2, 0)
-    image.paste(icon, icon_pos, icon)
-
-    # Load a custom TrueType font and Comandosuse it to overlay the key index, draw key
-    # label onto the image.
-    if titulo:
-        draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype(font_filename, 14)
-        label_w, label_h = draw.textsize(label_text, font=font)
-        label_pos = ((image.width - label_w) // 2, image.height - 20)
-        draw.text(label_pos, text=label_text, font=font, fill="white")
-    return PILHelper.to_native_format(deck, image)
-
-
-# Actualizar
-def CambiarImagen(deck, key, state, titulo = False):
-
-    # Obtener Imagen
-    ImagenEstilo = ObtenerImagen(deck, key, state)
-    #
-    Imagen = CargarImagen(deck, ImagenEstilo["icon"], ImagenEstilo["font"], ImagenEstilo["label"],titulo)
-
-    deck.set_key_image(key, Imagen)
-
-# Funciones de Teclado
-
 def PrecionarTecla(deck, key, state):
     global Estado
     if Estado == -1:
@@ -125,69 +58,78 @@ def PrecionarTecla(deck, key, state):
 
 def ComandoTeclas(Teclas):
     for tecla in Teclas:
-        keyboard.press(tecla)
+        if tecla == 'ctrl':
+            keyboard.press(Key.ctrl)
+        elif tecla == 'alt':
+            keyboard.press(Key.alt)
+        else:
+            keyboard.press(tecla)
 
     for tecla in Teclas:
-        keyboard.release(tecla)
+        if tecla == 'ctrl':
+            keyboard.press(Key.ctrl)
+        elif tecla == 'alt':
+            keyboard.press(Key.alt)
+        else:
+            keyboard.press(tecla)
 
-def ActualizarImagen(deck, teclas, tecla):
+def ActualizarImagen(deck, teclas, tecla, limpiar = False):
     global folder
+
     image = PILHelper.create_image(deck)
 
-    nombre = "{}".format(teclas[tecla]['Nombre'])
+    if not limpiar:
+        nombre = "{}".format(teclas[tecla]['Nombre'])
 
-    try:
-        print("Cambiando folder")
-        folder = "{}".format(teclas[tecla]['Folder'])
-    except KeyError:
-        print("No cambiar folder")
+        if 'ico' in teclas[tecla]:
+            NombreIcon = "{}".format(teclas[tecla]['ico'])
+        else:
+            if 'ico_defecto' in data:
+                NombreIcon = data['ico_defecto']
+            else:
+                NombreIcon = "imagen.png"
 
-    try:
-        NombreIcon = "{}/{}".format(folder,teclas[tecla]['ico'])
-        deck.set_brightness(data['Brillo'])
-    except KeyError:
-        NombreIcon = "IconoDefecto.png"
+        icon = Image.open(NombreIcon).convert("RGBA")
+        icon.thumbnail((image.width, image.height - 20), Image.LANCZOS)
+        icon_posicion = ((image.width - icon.width) // 2, 0)
+        image.paste(icon, icon_posicion, icon)
 
-    icon = Image.open(NombreIcon).convert("RGBA")
-    icon.thumbnail((image.width, image.height - 20), Image.LANCZOS)
-    icon_posicion = ((image.width - icon.width) // 2, 0)
-    image.paste(icon, icon_posicion, icon)
-
-    try:
-        titulo = "{}".format(teclas[tecla]['Titulo'])
-    except KeyError:
-        print(" ")
+        if 'Titulo' in teclas[tecla]:
+            titulo = "{}".format(teclas[tecla]['Titulo'])
+            dibujo = ImageDraw.Draw(image)
+            font = ImageFont.truetype(fuente, 14)
+            label_w, label_h = dibujo.textsize(titulo, font=font)
+            label_pos = ((image.width - label_w) // 2, image.height - 20)
+            dibujo.text(label_pos, text=titulo, font=font, fill="white")
 
     deck.set_key_image(tecla, PILHelper.to_native_format(deck, image))
 
 def ActualizarTeclas(deck, tecla, estado):
     global teclas
+
     if estado:
-        try:
-            print("Boton {} {} = {}".format(tecla, teclas[tecla]['Nombre'], estado), flush=True)
-            try:
+        if tecla < len(teclas):
+            print("Boton {} - {}".format(tecla, teclas[tecla]['Nombre']), flush=True)
+
+            if 'Regresar' in teclas[tecla]:
+                print("Regresar")
+                teclas = data['Comando']
+                for key in range(deck.key_count()):
+                    ActualizarImagen(deck, teclas, key, True)
+                for tecla in range(len(teclas)):
+                    ActualizarImagen(deck, teclas, tecla)
+            elif 'tecla' in teclas[tecla]:
                 print("comando {}".format(teclas[tecla]['tecla']))
-
-                for tecla in teclas[tecla]['tecla']:
-                    keyboard.press(tecla)
-
-                for tecla in teclas[tecla]['tecla']:
-                    keyboard.release(tecla)
-            except KeyError:
-                print("no comando")
-                try:
-                    #print(teclas[tecla]['key'])
-                    teclas = teclas[tecla]['key']
-                    #print(teclas)
-                    # for indice in range(len(teclas)):
-                    #     print(indice)
-                    #     ActualizarImagen(deck, teclas, indice)
-                except KeyError:
-                    print("no key")
-        except IndexError:
-            print("Boton {} No Asignada = {}".format(tecla, estado), flush=True)
-
-
+                ComandoTeclas(teclas[tecla]['tecla'])
+            else:
+                print("Entando a folder")
+                teclas = teclas[tecla]['Key']
+                for key in range(deck.key_count()):
+                    ActualizarImagen(deck, teclas, key, True)
+                for indice in range(len(teclas)):
+                    ActualizarImagen(deck, teclas, indice)
+        else:
+            print("Tecla no programada")
 
 # Principal
 if __name__ == "__main__":
@@ -206,19 +148,17 @@ if __name__ == "__main__":
         print("Abriendo '{}' dispositivo (Numero Serial: '{}')".format(deck.deck_type(), deck.get_serial_number()))
 
         # Cambiar Brillo
-        try:
+        if 'Brillo' in data:
             deck.set_brightness(data['Brillo'])
-        except KeyError:
+        else:
             deck.set_brightness(50)
 
-        try:
+        if 'Fuente' in data:
             fuente = "{}".format(data['Fuente'])
-        except KeyError:
-            print("Error faltar")
+        else:
+            print("Fuente no asignada")
+            deck.close()
 
-        # Activando Imagen Defecto
-        #for key in range(len(data['Comando'])):
-        #    CambiarImagen(deck, key, False)
 
         teclas = data['Comando']
         for tecla in range(len(teclas)):
