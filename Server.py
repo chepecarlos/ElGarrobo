ddfa7734c887afe8dba0e36ff4a87f10475ff914
ChepeCarlos <chepecarlos@alswblog.org>
2020-03-04 21:27:33 -0600

    prueva del servidor

--- Server.py
+++ Server.py
@@ -3,14 +3,57 @@
 import asyncio
 import websockets
 import os
+from pynput.keyboard import Key, Controller
+
+# Cargar Teclado
+keyboard = Controller()
 
 print("Activando Servidor")
+
+def ComandoTeclas(Teclas):
+
+    for tecla in Teclas:
+        if tecla == 'ctrl':
+            keyboard.press(Key.ctrl)
+        elif tecla == 'alt':
+            keyboard.press(Key.alt)
+        elif tecla == 'shift':
+            keyboard.press(Key.shift)
+        elif tecla == 'super':
+            keyboard.press(Key.cmd)
+        elif tecla == 'f9':
+            keyboard.press(Key.f9)
+        elif tecla == 'f10':
+            keyboard.press(Key.f10)
+        else:
+            keyboard.press(tecla)
+
+    for tecla in Teclas:
+        if tecla == 'ctrl':
+            keyboard.release(Key.ctrl)
+        elif tecla == 'alt':
+            keyboard.release(Key.alt)
+        elif tecla == 'shift':
+            keyboard.release(Key.shift)
+        elif tecla == 'super':
+            keyboard.release(Key.cmd)
+        elif tecla == 'f9':
+            keyboard.release(Key.f9)
+        elif tecla == 'f10':
+            keyboard.release(Key.f10)
+        else:
+            keyboard.release(tecla)
+
 
 async def comandoOS(websocket, path):
     comando = await websocket.recv()
     print(f"< {comando}")
-
-    os.system(comando)
+    Separar = comando.split()
+    if(Separar[0] == 'Key'):
+        Separar.remove('Key')
+        ComandoTeclas(Separar)
+    else:
+        os.system(comando)
 
     Respuesta = f"Ejacutando {comando}!"
 
