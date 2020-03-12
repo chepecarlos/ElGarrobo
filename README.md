72bc096b95d538bb17e4a003338133dfcd5ebe70
ChepeCarlos <chepecarlos@alswblog.org>
2020-03-11 20:17:13 -0600

    Cambiando librerias de teclado

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Configuraciones para ElGato stream-deck de ALSW escrito en Python
 * sudo apt update
 * sudo apt upgrade
 * sudo apt install python3-pip
+* sudo apt-get install scrot python3-tk python3-dev
 * pip3 install websockets
-* pip3 install pynput
+* pip3 install pyautogui
 
 # TODO
 
@@ -15,3 +16,7 @@ Configuraciones para ElGato stream-deck de ALSW escrito en Python
 * Inocos para submenos
 * Instalador
 * Documentacion
+
+# Problemas
+
+* Precionar tecla Super
--- ServerPy/Server.py
+++ ServerPy/Server.py
@@ -3,7 +3,7 @@
 import asyncio
 import websockets
 import os
-from pynput.keyboard import Key, Controller
+import pyautogui
 
 # Cargar Teclado
 keyboard = Controller()
@@ -13,54 +13,24 @@ print("Activando Servidor")
 def ComandoTeclas(Teclas):
 
     for tecla in Teclas:
-        if tecla == 'ctrl':
-            keyboard.press(Key.ctrl)
-        elif tecla == 'alt':
-            keyboard.press(Key.alt)
-        elif tecla == 'shift':
-            keyboard.press(Key.shift)
-        elif tecla == 'super':
-            keyboard.press(Key.cmd)
-        elif tecla == 'f9':
-            keyboard.press(Key.f9)
-        elif tecla == 'f10':
-            keyboard.press(Key.f10)
-        else:
-            keyboard.press(tecla)
+        pyautogui.keyDown(tecla)
 
-    for tecla in Teclas:
-        if tecla == 'ctrl':
-            keyboard.release(Key.ctrl)
-        elif tecla == 'alt':
-            keyboard.release(Key.alt)
-        elif tecla == 'shift':
-            keyboard.release(Key.shift)
-        elif tecla == 'super':
-            keyboard.release(Key.cmd)
-        elif tecla == 'f9':
-            keyboard.release(Key.f9)
-        elif tecla == 'f10':
-            keyboard.release(Key.f10)
-        else:
-            keyboard.release(tecla)
+    for tecla in reversed(Teclas):
+        pyautogui.keyUp(tecla)
 
 async def comandoOS(websocket, path):
     comando = await websocket.recv()
     print(f"< {comando}")
     Separar = comando.split()
-    Respuesta = ''
     if(Separar[0] == 'Key'):
         Separar.remove('Key')
-        Respuesta = f"Teclas {Separar}"
         ComandoTeclas(Separar)
     else:
-        Respuesta = f"Ejacutando {comando}!"
         os.system(comando)
-
-    await websocket.send(Respuesta)
+    # await websocket.send(Respuesta)
     print(f"> {Respuesta}")
 
-start_server = websockets.serve(comandoOS, "Ryuk.local", 8765)
+start_server = websockets.serve(comandoOS, "ryuk.local", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
