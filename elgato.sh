9c3812d244b44fd5800aee9d5d017a029cf31228
ChepeCarlos <chepecarlos@alswblog.org>
2020-04-12 11:25:43 -0600

    mejorando scrip para cliente y servidor

--- elgato.sh
+++ elgato.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-echo Cargando El Gato
-cd $HOME/Programa/ElGatoALSW
-python3 ElGatoALSW.py
+# TODO: Agregar mas opciones al lanzdor
+# TODO: Repara eror de lanzar de otro lado k
+
+
+while [ -n "$1" ]; do
+	case "$1" in
+	-c)
+      echo "Cargando El Gato"
+      cd $HOME/Programa/ElGatoALSW
+      python3 ElGatoALSW.py;;
+	-s)
+      echo "Cargando Servidor"
+      cd $HOME/Programa/ElGatoALSW/ServerPy
+      python3 Server.py;;
+	*) echo "No hay opcion para $1" ;; #
+  ":")
+        echo "Sin valor de argumentos para la opción $OPTARG"
+        ;;
+	esac
+	shift
+done
