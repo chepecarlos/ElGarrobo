734c9837d66080df18b75b176fc1aac40655993f
chepecarlos <chepecarlos@alswblog.org>
2020-11-16 10:07:27 -0600

    Reparando problema cargando funciones de teclado base

--- Extra/CargarData.py
+++ Extra/CargarData.py
@@ -3,19 +3,21 @@ import json
 import sys
 from Extra.Depuracion import Imprimir
 
+
 def CargarData(Direcion):
     """Carga Archivo de comandos y Raton"""
     Data = CargarValores(Direcion)
-    if 'CargarData' in Data:
+    if 'CargandoRaton' in Data:
         Data['teclado'] = CargarValores(Data['CargandoRaton'])
     if 'CargandoComando' in Data:
         Data['Comando'] = CargarValores(Data['CargandoComando'])
-        for i in range(len(Data['Comando'])):
-            if 'Cargar' in Data['Comando'][i]:
-                Data['Comando'][i]['Key'] = CargarValores(Data['Comando'][i]['Cargar'])
-            if 'CargandoRaton' in Data['Comando'][i]:
-                Data['Comando'][i]['teclado'] = CargarValores(Data['Comando'][i]['CargandoRaton'])
+    for i in range(len(Data['Comando'])):
+        if 'Cargar' in Data['Comando'][i]:
+            Data['Comando'][i]['Key'] = CargarValores(Data['Comando'][i]['Cargar'])
+        if 'CargandoRaton' in Data['Comando'][i]:
+            Data['Comando'][i]['teclado'] = CargarValores(Data['Comando'][i]['CargandoRaton'])
     return Data
+
 
 def CargarValores(Direcion):
     """Cargando Data de un Archivo"""
