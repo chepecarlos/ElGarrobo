2646519d332f4aba9e94fe0163b12d9cc304cb6c
chepecarlos <chepecarlos@alswblog.org>
2020-12-28 12:55:38 -0600

    Quitando funciones del archivo pincipal que fuero movidad a archivos propios

--- ElGatoALSW.py
+++ ElGatoALSW.py
@@ -1,41 +1,20 @@
 #!/usr/bin/env python3
 # TODO: agregar git https://python-elgato-streamdeck.readthedocs.io/en/stable/examples/animated.html
-# TODO: Agregar siquiente automaticamente
-# TODO: Reordenar codigo
-# TODO: Emular Raton
-# BUG: Error cargando sin folder
 # Librerias
 import os
-import threading
-# Librerias para idenficiar Teclado
-from evdev import InputDevice, categorize, ecodes
 import argparse
 
-import Extra.FuncionesOBS as OBSWebSocketPropios
-import Extra.MiMQTT as MiMQTTs
 import Extra.MiDeck as MiDecks
 
 # Cargar funciones de Archivos
-from Extra.FuncionesProyecto import SalvarProyecto, CargarProyecto, AbirProyecto, CargarIdVideo
+from Extra.FuncionesProyecto import SalvarProyecto, CargarProyecto, CargarIdVideo
 from Extra.News import SalvarArchivoNoticia
-from Extra.EmularTeclado import ComandoTeclas, ComandoEscribir
 from Extra.Depuracion import Imprimir, CambiarDepuracion
 from Extra.YoutubeChat import SalvarChatYoutube
 from Extra.CargarData import CargarData
 from Extra.Hilos import CargarHilo
 
-# TODO: ordenar para no usar variable globales
-MiDeck = "nada"
-teclas = "nada"
-ComandosRaton = "nada"
-folder = ""
-fuente = ""
-data = ""
-DefaceBotones = 0
 
-MiOBS = OBSWebSocketPropios.MiObsWS()
-MiMQTT = MiMQTTs.MiMQTT()
-# TODO: Agregar sonidos
 parser = argparse.ArgumentParser(description='Heramienta de creacion de contenido de ALSW')
 parser.add_argument('--master', '-m', help="Cargar servidor de %(prog)s",  action="store_true")
 parser.add_argument('--cliente', '-c', help="Cargando cliente de %(prog)s",  action="store_true")
@@ -47,37 +26,6 @@ parser.add_argument('--noticias', '-n', help="Configurar folder a noticias actua
 parser.add_argument('--salvaryoutube', '-sy', help="Salva el chat en un archivo", action="store_true")
 parser.add_argument('--deckdemo', '-dd', help="Solo usar StreamDeck",  action="store_true")
 
-#  Codigo del Raton_Razer
-
-
-def CargandoRaton():
-    '''Cargas las configuraciones del Teclado Ratom'''
-    global data
-    global ComandosRaton
-    Imprimir("Cargando Raton Razer")
-    ComandosRaton = data['teclado']
-    if 'Raton_Razer' in data:
-        Raton = InputDevice(data['Raton_Razer'])
-        Raton.grab()
-        HiloRazer = threading.Thread(target=HiloRaton, args=(Raton,), daemon=True)
-        HiloRazer.start()
-    else:
-        Imprimir("error Raron Razer no definido")
-
-
-def HiloRaton(Raton):
-    '''Hila del teclado del Raton'''
-    global ComandosRaton
-    for event in Raton.read_loop():
-        if event.type == ecodes.EV_KEY:
-            key = categorize(event)
-            if key.keystate == key.key_down:
-                for teclas in ComandosRaton:
-                    if 'Boton' in teclas:
-                        if teclas['Boton'] == key.keycode:
-                            Imprimir(f"Raton {key.keycode} - {teclas['Nombre']}")
-                            ActualizarAccion(teclas)
-
 
 # Principal
 if __name__ == "__main__":
@@ -88,7 +36,7 @@ if __name__ == "__main__":
     if args.master:
         Imprimir("Modo Master")
         data = CargarData('Comandos.json')
-        CargandoRaton()
+        # CargandoRaton()
         # CargandoElGato()
         CargarHilo()
     elif args.cliente:
@@ -101,7 +49,7 @@ if __name__ == "__main__":
     elif args.ratom:
         Imprimir("Modo Solo Raton Razer")
         data = CargarData('Comandos.json')
-        CargandoRaton()
+        # CargandoRaton()
         CargarHilo()
     elif args.proyecto:
         Imprimir("Configurando Folder como Proyecto Actual")
@@ -120,6 +68,5 @@ if __name__ == "__main__":
     else:
         Imprimir("No parametro")
         data = CargarData('Comandos.json')
-        CargandoRaton()
-        # CargandoElGato()
+        Deck = MiDecks.MiDeck(data)
         CargarHilo()
