402f49d374b69136edbf1b1586c940636a0e5268
chepecarlos <chepecarlos@alswblog.org>
2023-05-20 23:07:00 -0600

    limpiando codigo de mqtt

--- src/acciones/mqtt.py
+++ src/acciones/mqtt.py
@@ -14,36 +14,17 @@ def mensajeMQTT(opciones):
     topic -> stl
         tema a publicar el mensaje
     """
-    Mensaje = None
-    Topic = None
-    Usuario = None
-    Contrasenna = None
-    Servidor = None
-    Puerto = None
-    Esperar = None
-    if "mensaje" in opciones:
-        Mensaje = opciones["mensaje"]
-    elif "opciones" in opciones:
+    Mensaje = opciones.get("mensaje")
+    Topic = opciones.get("topic")
+    Usuario = opciones.get("usuario")
+    Servidor = opciones.get("servidor")
+    Puerto = opciones.get("puerto")
+    Contrasenna = opciones.get("contrasenna")
+    Esperar = opciones.get("esperar")
+
+    if "opciones" in opciones:
         Mensaje = opciones["opciones"]
         Mensaje = json.dumps(Mensaje)
-
-    if "topic" in opciones:
-        Topic = opciones["topic"]
-
-    if "usuario" in opciones:
-        Usuario = opciones["usuario"]
-
-    if "servidor" in opciones:
-        Servidor = opciones["servidor"]
-
-    if "puerto" in opciones:
-        Puerto = opciones["puerto"]
-
-    if "contrasenna" in opciones:
-        Contrasenna = opciones["contrasenna"]
-
-    if "esperar" in opciones:
-        Esperar = opciones["esperar"]
 
     if Mensaje is not None and Topic is not None:
         if Esperar is None or not Esperar:
