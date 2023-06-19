7dbd14dc8c5512bc82230c8d8a8038ef617f4837
chepecarlos <chepecarlos@alswblog.org>
2023-06-19 10:39:46 -0600

    actualizando la funcion de streamdeck

--- src/dispositivos/mideck/mi_streamdeck.py
+++ src/dispositivos/mideck/mi_streamdeck.py
@@ -1,31 +1,32 @@
 # https://python-elgato-streamdeck.readthedocs.io/en/stable/index.html
 
+import time
+
+from MiLibrerias import ConfigurarLogging, ObtenerValor
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.Transport.Transport import TransportError
 
+from .mi_deck_extra import BuscarDirecionImagen
 from .mi_deck_gif import DeckGif
 from .mi_deck_imagen import ActualizarIcono, LimpiarIcono
-from .mi_deck_extra import BuscarDirecionImagen
-
-from MiLibrerias import ConfigurarLogging
-from MiLibrerias import ObtenerValor
 
 logger = ConfigurarLogging(__name__)
 
 
 class MiStreamDeck(object):
-
     def __init__(self, Data, Evento, Base):
-        self.ID = Data['id']
-        self.Nombre = Data['nombre']
-        self.Serial = Data['serial']
-        self.File = Data['file']
+        self.ID = Data["id"]
+        self.Nombre = Data["nombre"]
+        self.Serial = Data["serial"]
+        self.File = Data["file"]
         self.Conectado = False
         self.Deck = None
         self.DeckGif = None
         self.Cantidad = 0
         self.Base = Base
         self.Evento = Evento
+        self.ultimoDibujo = None
+        self.tiempoDibujar = 0.4
 
     def Conectar(self):
         streamdecks = DeviceManager().enumerate()
@@ -40,6 +41,7 @@ class MiStreamDeck(object):
                     self.Conectado = True
                     self.Cantidad = self.Deck.key_count()
                     Brillo = ObtenerValor("data/streamdeck.json", "brillo")
+                    # Todo: brillo no es un int
                     self.Deck.set_brightness(Brillo)
                     self.DeckGif = DeckGif(self.Deck)
                     self.DeckGif.start()
@@ -58,24 +60,37 @@ class MiStreamDeck(object):
                 logger.exception(f"Error 2 {error}")
 
     def ActualizarIconos(self, acciones, desface, Unido=False):
-        """Refesca iconos, tomando en cuenta pagina actual."""
-        if self.Conectado:
-            logger.info(f"Deck[Dibujar] {self.Nombre}")
-            if Unido:
-                for i in range(self.Cantidad):
-                    key_desface = i + self.Base + desface
-                    Dibujar = list(filter(lambda accion: accion['key'] == key_desface, acciones))
-                    # TODO: Cargar primer los Gifs # if AccionAcual is not None:
-                    
-                    if Dibujar:
-                        AccionAcual = Dibujar[0]
-                        DirecionImagen = BuscarDirecionImagen(AccionAcual)
-                        if DirecionImagen is not None and DirecionImagen.endswith(".gif"):
-                            self.DeckGif.ActualizarGif(i, AccionAcual)
-                        else:
-                            ActualizarIcono(self.Deck, i, AccionAcual)
-            else:
-                pass
+        """Refresca iconos, tomando en cuenta pagina actual."""
+        if not self.Conectado:
+            return
+
+        if self.Deck is None or not self.Deck.is_open():
+            self.Conectado = False
+            return
+
+        tiempoActual = time.time()
+
+        if self.ultimoDibujo is None:
+            self.ultimoDibujo = -self.tiempoDibujar
+
+        if tiempoActual - self.ultimoDibujo < self.tiempoDibujar:
+            print(tiempoActual - self.ultimoDibujo, self.tiempoDibujar)
+            return
+
+        logger.info(f"Deck[Dibujar] {self.Nombre}")
+        if Unido:
+            for i in range(self.Cantidad):
+                key_desface = i + self.Base + desface
+                dibujar = list(filter(lambda accion: accion["key"] == key_desface, acciones))
+                # TODO: Cargar primer los Gifs # if AccionAcual is not None:
+
+                if dibujar:
+                    AccionAcual = dibujar[0]
+                    DirecionImagen = BuscarDirecionImagen(AccionAcual)
+                    if DirecionImagen is not None and DirecionImagen.endswith(".gif"):
+                        self.DeckGif.ActualizarGif(i, AccionAcual)
+                    else:
+                        ActualizarIcono(self.Deck, i, AccionAcual)
 
     def Limpiar(self):
         """Borra iconos de todo los botones de StreamDeck."""
@@ -92,16 +107,12 @@ class MiStreamDeck(object):
     def CambiarFolder(self, Folder):
         if self.Conectado:
             self.Deck.Folder = Folder
+            self.ultimoDibujo = -self.tiempoDibujar
 
     def ActualizarBoton(self, Deck, Key, Estado):
-        data = {"nombre": self.Nombre,
-                "key": Key,
-                "deck": True,
-                "base": self.Base,
-                "estado": Estado
-                }
+        data = {"nombre": self.Nombre, "key": Key, "deck": True, "base": self.Base, "estado": Estado}
         self.Evento(data)
-    
+
     def Desconectar(self):
         if self.Conectado:
             logger.info(f"Deck[Desconectando] - {self.Nombre}")
@@ -110,13 +121,13 @@ class MiStreamDeck(object):
             self.Deck.reset()
             self.Deck.close()
 
+
 def IniciarStreamDeck(Datas, FuncionEvento):
     streamdecks = DeviceManager().enumerate()
-    logger.info(
-        f"Cargando StreamDeck - {len(streamdecks) if len(streamdecks) > 0 else 'No Conectado'}")
+    logger.info(f"Cargando StreamDeck - {len(streamdecks) if len(streamdecks) > 0 else 'No Conectado'}")
     ListaDeck = []
     for Data in Datas:
-        Data['Encontado'] = False
+        Data["Encontado"] = False
 
     for deck in streamdecks:
         DeckActual = deck
@@ -126,18 +137,17 @@ def IniciarStreamDeck(Datas, FuncionEvento):
         DeckActual.set_brightness(Brillo)
 
         for Data in Datas:
-            if Data['serial'] == DeckActual.get_serial_number():
-                logger.info(
-                    f"Conectando: {Data['nombre']} - {DeckActual.get_serial_number()}")
+            if Data["serial"] == DeckActual.get_serial_number():
+                logger.info(f"Conectando: {Data['nombre']} - {DeckActual.get_serial_number()}")
                 DeckActual.Serial = DeckActual.get_serial_number()
                 DeckActual.Cantidad = DeckActual.key_count()
-                DeckActual.ID = Data['id']
-                DeckActual.Nombre = Data['nombre']
-                DeckActual.File = Data['file']
+                DeckActual.ID = Data["id"]
+                DeckActual.Nombre = Data["nombre"]
+                DeckActual.File = Data["file"]
                 DeckActual.FuncionEvento = FuncionEvento
                 DeckActual.set_key_callback(ActualizarBoton)
                 ListaDeck.append(DeckActual)
-                Data['encontado'] = True
+                Data["encontado"] = True
 
     ListaDeck.sort(key=lambda x: x.ID, reverse=False)
     Cantidad_Base = 0
@@ -146,17 +156,11 @@ def IniciarStreamDeck(Datas, FuncionEvento):
         Cantidad_Base += deck.Cantidad
 
     for Data in Datas:
-        if not Data['encontado']:
-            logger.warning(
-                f"No se encontro: {Data['nombre']} - {Data['serial']}")
+        if not Data["encontado"]:
+            logger.warning(f"No se encontro: {Data['nombre']} - {Data['serial']}")
     return ListaDeck
 
 
 def ActualizarBoton(Deck, Key, Estado):
-    data = {"nombre": Deck.Nombre,
-            "key": Key,
-            "deck": True,
-            "base": Deck.Base,
-            "estado": Estado
-            }
+    data = {"nombre": Deck.Nombre, "key": Key, "deck": True, "base": Deck.Base, "estado": Estado}
     Deck.FuncionEvento(data)
