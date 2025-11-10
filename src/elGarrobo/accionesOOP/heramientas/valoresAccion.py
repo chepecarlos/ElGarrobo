e66e76e7bb35669a28b1d486b3fe78c919f027ba
chepecarlos <chepecarlos@alsw.net>
2025-11-09 20:48:58 -0600

    convirtiendo en dataClass de valoresAcciones

--- src/elGarrobo/accionesOOP/heramientas/valoresAccion.py
+++ src/elGarrobo/accionesOOP/heramientas/valoresAccion.py
@@ -1,10 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class valoresAcciones:
+
     atributo: str
     valor: any
-
-    def __init__(self, atributo: str, valor: any) -> None:
-        self.atributo: str = atributo
-        self.valor: any = valor
 
     def __str__(self) -> str:
         return f"{self.atributo} = {self.valor}"
