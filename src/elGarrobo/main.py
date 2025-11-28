77eed10a506b7d5a47f096dc3e5ba2db4b79745e
chepecarlos <chepecarlos@alsw.net>
2025-11-27 18:36:22 -0600

    agregando --version

--- src/elGarrobo/main.py
+++ src/elGarrobo/main.py
@@ -5,18 +5,22 @@ Este es el Inicio del Código que llama a las funciones
 import argparse
 
 from configurar.modulo import ConfigurarModulos
+from elGarrobo import miLibrerias
 from elGarrobo.elGarrobo import elGarrobo
 from elGarrobo.miLibrerias import ConfigurarLogging, ObtenerFolderConfig
 
 logger = ConfigurarLogging(__name__)
 
 
-def parametros() -> argparse.Namespace:
+def argumentos() -> argparse.Namespace:
 
     parser = argparse.ArgumentParser(description="Herramientas de Macros de ALSW")
     parser.add_argument("--gui", "-g", help="Sistema interface gráfica", action="store_true")
     parser.add_argument("--configurar", "-c", help="Sistema configuración del programa", action="store_true")
     parser.add_argument("--depuracion", "-d", help="Activa la depuración", action="store_true")
+
+    __version__ = miLibrerias.obtenerVersionPaquete()
+    parser.add_argument("--version", action="version", help="Version del programa", version=f"%(prog)s {__version__}")
 
     return parser.parse_args()
 
@@ -29,7 +33,7 @@ def configurar() -> None:
 def main() -> None:
 
     logger.info("ElGarrobo[Iniciando]")
-    args = parametros()
+    args = argumentos()
     if args.depuracion:
         logger.info("ElGarrobo[Depuración Activa]")
         logger.setLevel("DEBUG")
