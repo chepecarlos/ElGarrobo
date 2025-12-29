449d940c37a8bbbf8b3943aa3ed0f8e77cd8f2a7
chepecarlos <chepecarlos@alsw.net>
2025-12-28 20:06:49 -0600

    agregando funciones base para dispositivo StreamDeck plus

--- src/elGarrobo/dispositivos/mideck/mi_streamdeck_plus.py
+++ src/elGarrobo/dispositivos/mideck/mi_streamdeck_plus.py
@@ -1,3 +1,5 @@
+from StreamDeck.Devices.StreamDeck import DialEventType, TouchscreenEventType
+
 from elGarrobo.dispositivos.mideck.mi_streamdeck import MiStreamDeck
 
 
@@ -15,3 +17,31 @@ class MiStreamDeckPlus(MiStreamDeck):
         """
         super().__init__(dataConfiguracion)
         self.nombre = dataConfiguracion.get("nombre", "streamdeckplus")
+
+    def conectar(self) -> None:
+
+        super().conectar()
+
+        if self.conectado:
+            self.deck.set_dial_callback(self.actualizarEncoderRotatorio)
+            self.deck.set_touchscreen_callback(self.actualizarTouchScreen)
+
+    def actualizarEncoderRotatorio(self, deck, dial, event, value):
+        if event == DialEventType.PUSH:
+            print(f"dial pushed: {dial+1} state: {value}")
+        elif event == DialEventType.TURN:
+            print(f"dial {dial+1} turned: {value}")
+        elif event == DialEventType.TURN:
+            print(f"dial {dial+1} turned: {value}")
+
+    def actualizarTouchScreen(self, deck, evt_type, value):
+        if evt_type == TouchscreenEventType.SHORT:
+            print("Short touch @ " + str(value["x"]) + "," + str(value["y"]))
+
+        elif evt_type == TouchscreenEventType.LONG:
+
+            print("Long touch @ " + str(value["x"]) + "," + str(value["y"]))
+
+        elif evt_type == TouchscreenEventType.DRAG:
+
+            print("Drag started @ " + str(value["x"]) + "," + str(value["y"]) + " ended @ " + str(value["x_out"]) + "," + str(value["y_out"]))
